db83858db4652b6e8513a507b88085509d765076
Syntaf <gmercer015@gmail.com>
2020-07-24 23:22:25 +0000

    Write ajax submit for forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,41 @@ require('./bootstrap');
 
 class FormController
 {
-    sayHi () {
-        console.log('Hello there');
+    constructor(formId) {
+        this.$form = $(formId);
+
+        this.initialize();
+    }
+
+    initialize () {
+        this.$form.on('submit', this.handleSubmit.bind(this));
+    }
+
+    handleSubmit (event) {
+        event.preventDefault();
+
+        const data = this.$form.serialize();
+        const url = this.$form.attr('action');
+
+        this.postForm(url, data)
+            .done(this.handleSuccess.bind(this))
+            .fail(this.handleFailure.bind(this));
+    }
+
+    postForm (url, data) {
+        return $.ajax({
+            method: 'POST',
+            url: url,
+            data: data
+        });
+    }
+
+    handleSuccess (res) {
+        console.log(res.responseJSON);
+    }
+
+    handleFailure (res) {
+        console.log(res.responseJSON);
     }
 }
 
