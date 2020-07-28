045c943791b963523a7f3adc4df2ba1dc1a55dc5
Syntaf <gmercer015@gmail.com>
2020-07-28 06:01:11 +0000

    Add message_lines attribute on Inquiry

--- app/Inquiry.php
+++ app/Inquiry.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inquiry extends Model
 {
+    private const DEFAULT_LINE_SIZE = 76;
+
     protected $fillable = ['full_name', 'email', 'phone_number', 'message'];
+
+    public function getMessageLinesAttribute()
+    {
+        return str_split($this->message, self::DEFAULT_LINE_SIZE);
+    }
 }
